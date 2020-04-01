d83ba1eab6cf1252763b5ac3e49eabfb039d8d17
loje75@gmail.com <loje75@gmail.com>
2020-04-02 00:35:01 +0900

    add test code

---
+++ api/users/user.spec.js
@@ -0,0 +1,71 @@
+const should = require('should');
+const request = require('supertest');
+const app = require('../../app');
+
+describe('Test', () => {
+  it('GET /users', (done) => {
+    request(app)
+      .get('/users')
+      .expect(200)
+      .end((err, res) =>{
+        if (err) {
+          throw err;
+        }
+        done();
+      });
+  });
+
+  it('GET /users', (done) => {
+    request(app)
+      .get('/users')
+      .end((err, res) => {
+        if (err) {
+          throw err;
+        }
+        res.body.should.be.an.instanceOf(Array).and.have.length(3);
+        res.body.map(user => {
+          user.should.have.properties('id', 'name');
+          user.id.should.be.a.Number();
+          user.name.should.be.a.String();
+        });
+        done();
+      });
+  });
+
+  it('GET /users/:id', (done) => {
+    request(app)
+    .get('/users/1')
+    .expect(200)
+    .end((err, res) =>{
+      if (err) {
+        throw err;
+      }
+      done();
+    });
+  });
+
+  it('POST /users', (done) => {
+    request(app)
+      .post('/users')
+      .send({name: 'Ahn'})
+      .expect(201)
+      .end((err, res) => {
+        if (err) {
+          throw err;
+        }
+        done();
+      });
+  });
+
+  it('DELETE /users/:id', (done) => {
+    request(app)
+      .delete('/users/1')
+      .expect(204)
+      .end((err, res) => {
+        if (err) {
+          throw err;
+        }
+        done();
+      });
+  });
+});
--- app.js
+++ app.js
@@ -4,4 +4,6 @@ const app = express();
 app.use('/users', require('./api/users'));
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
-})
+});
+
+module.exports = app;
